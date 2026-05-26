fed04ce5dd0c6d139280e376659924fef6b9fdf7
goo-sle <enfl7500@gmail.com>
2026-05-26 14:17:27 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,31 @@
+const CACHE = 'osaka26-v1';
+
+self.addEventListener('install', e => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  const url = new URL(e.request.url);
+  if (url.origin !== self.location.origin) return;
+
+  e.respondWith(
+    fetch(e.request)
+      .then(res => {
+        if (res.ok) {
+          caches.open(CACHE).then(c => c.put(e.request, res.clone()));
+        }
+        return res;
+      })
+      .catch(() => caches.match(e.request))
+  );
+});
